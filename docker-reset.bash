4af5b071623f15b282e621461eb607097543a8de
Nick Yeoman <c@nickyeoman.com>
2020-05-18 20:08:26 -0700

    added matomo

--- docker-reset.bash
+++ docker-reset.bash
@@ -3,22 +3,24 @@
 # Docker clean up guilde https://gist.github.com/bastman/5b57ddb3c11942094f8d0a97d461b430
 
 # kill all containers
+echo "*** Killing All Containers ***"
 docker stop $(docker ps -aq)
 
+echo "*** Docker prune ***"
 docker system prune
 
-#remove all containers
-#docker rm $(docker ps -aq)
-
 #remove all images
-#docker rmi $(docker images -q)
+echo "*** Removing all Docker images ***"
+docker rmi $(docker images -q)
 
 #delete all volumnes
-#docker volume rm $(docker volume ls -qf dangling=true)
-#docker volume ls -qf dangling=true | xargs -r docker volume rm
+echo "*** Removing all Docker volumes ***"
+docker volume rm $(docker volume ls -qf dangling=true)
+docker volume ls -qf dangling=true | xargs -r docker volume rm
 
 #remove all networks
+echo "*** Removing all Docker Networks ***"
 docker network prune
-#docker network ls
-#docker network ls | grep "bridge"
-#docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+
+echo "*** All done ***"
